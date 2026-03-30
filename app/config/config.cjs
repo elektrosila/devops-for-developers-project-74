2d5702fcec566be8d4dc94092a1404ccfb5e9f76
elektrosila <146366637+elektrosila@users.noreply.github.com>
2026-03-30 22:14:12 +0300

    Update config.cjs

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -5,30 +5,16 @@ module.exports = {
     dialect: 'sqlite',
     storage: './database.sqlite',
   },
-  production: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
   test: {
     dialect: 'sqlite',
     storage: './database.test.sqlite',
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
+  production: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    dialect: 'postgres',
+  },
 };
