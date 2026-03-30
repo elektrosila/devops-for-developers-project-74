ae9e6d87ed70639506422a2165cc7d0ec5d3e3a1
elektrosila <146366637+elektrosila@users.noreply.github.com>
2026-03-30 21:51:09 +0300

    Update config.cjs

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const useSsl = process.env.DATABASE_SSL === 'true';
+
 module.exports = {
   development: {
     dialect: 'sqlite',
@@ -10,25 +12,19 @@ module.exports = {
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    host: process.env.DATABASE_HOST || 'db',
+    ...(useSsl ? {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
-    },
+    } : {}),
   },
   test: {
     dialect: 'sqlite',
     storage: './database.test.sqlite',
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
 };
